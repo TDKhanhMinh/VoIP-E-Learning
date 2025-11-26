e7f235383f9bf9ef5fe3c3751a3c25dc9351af6c
meouisme <118751514+meouisme@users.noreply.github.com>
2025-11-26 21:06:36 +0700

    fix: Correct import path for DescriptionCopy component

--- frontend/src/components/Tests/OnlineTestCard.jsx
+++ frontend/src/components/Tests/OnlineTestCard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import { attemptService } from "../../services/attemptService";
 import { Link, useNavigate } from "react-router-dom";
-import DescriptionCopy from './../common/DescriptionCopy';
+import DescriptionCopy from './../Common/DescriptionCopy';
 
 const OnlineTestCard = ({ data, role = "student", refreshKey }) => {
   const navigate = useNavigate();
