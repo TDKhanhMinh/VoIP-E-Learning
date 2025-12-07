0a81b2b8b24e6fc9a2ce8f43f69f32a392d5af96
meouisme <118751514+meouisme@users.noreply.github.com>
2025-12-07 21:20:10 +0700

    feat: Fix connection object structure in jobQueueService for REDIS_URL

--- backend/src/service/jobQueueService.js
+++ backend/src/service/jobQueueService.js
@@ -1,6 +1,7 @@
 import { Queue } from "bullmq";
+
 const connection = process.env.REDIS_URL
-  ? { connection: { url: process.env.REDIS_URL } }
+  ? { url: process.env.REDIS_URL }
   : {
       host: process.env.REDIS_HOST || "localhost",
       port: 6379,
